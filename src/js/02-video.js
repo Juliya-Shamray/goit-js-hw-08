30c0a50baf084115b5ca783a14e56d8b57b87b26
Juliya Shamray <fizmatka908990@gmail.com>
2023-08-11 14:05:07 +0300

    added task №2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const KEY_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem(KEY_TIME, JSON.stringify(data));
+};
+
+console.log(JSON.parse(localStorage.getItem(KEY_TIME)).seconds);
+player.setCurrentTime(JSON.parse(localStorage.getItem(KEY_TIME)).seconds);
+
+player.on('timeupdate', throttle(onPlay, 1000));
